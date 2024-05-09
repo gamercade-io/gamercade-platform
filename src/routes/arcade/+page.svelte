b11ba6b0d3f88e2c4811accc351bea39add7a677
RobDavenport <zerve0@hotmail.com>
2024-05-09 16:54:20 +0900

    add centering

--- src/routes/arcade/+page.svelte
+++ src/routes/arcade/+page.svelte
@@ -1,1 +1,18 @@
-<h1>Arcade</h1>
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<main class="container">
+	<label>
+		Username
+		<input name="username" placeholder="Username" />
+	</label>
+
+	<label for="Password">Password</label>
+	<input type="password" id="password" placeholder="Password" />
+
+	<button>Login</button>
+	<a href="/arcade/register" role="button">Register</a>
+</main>
--- src/routes/arcade/register/+page.svelte
+++ src/routes/arcade/register/+page.svelte
@@ -4,22 +4,24 @@
 	export let data: PageData;
 </script>
 
-<label>
-	Username
-	<input name="username" placeholder="Username" />
-</label>
+<main class="container">
+	<label>
+		Username
+		<input name="username" placeholder="Username" />
+	</label>
 
-<label for="Password">Password</label>
-<input type="password" id="password" placeholder="Password" />
+	<label for="Password">Password</label>
+	<input type="password" id="password" placeholder="Password" />
 
-<label for="Password Confirmation">Password</label>
-<input type="password" id="passwordConfirm" placeholder="Password Confirmation" />
+	<label for="Password Confirmation">Password</label>
+	<input type="password" id="passwordConfirm" placeholder="Password Confirmation" />
 
-<label for="Email">Email</label>
-<input type="email" id="email" placeholder="Email" />
+	<label for="Email">Email</label>
+	<input type="email" id="email" placeholder="Email" />
 
-<label for="Email Confirmation">Email Confirmation</label>
-<input type="email" id="emailConfirm" placeholder="Email Confirmation" />
+	<label for="Email Confirmation">Email Confirmation</label>
+	<input type="email" id="emailConfirm" placeholder="Email Confirmation" />
 
-<a href="/arcade/login" role="button">Back</a>
-<button>Create</button>
+	<a href="/arcade" role="button">Back</a>
+	<button>Create</button>
+</main>
