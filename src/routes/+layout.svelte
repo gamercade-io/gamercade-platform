59036f00b519e4469e966e5cc58f3ae0fa6b7398
RobDavenport <zerve0@hotmail.com>
2024-05-10 18:47:52 +0900

    add tauri

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import '@picocss/pico/css/pico.indigo.min.css';
 	import NavBar from '../NavBar.svelte';
+
+	export const prerender = true;
+	export const ssr = false;
 </script>
 
 <NavBar />
