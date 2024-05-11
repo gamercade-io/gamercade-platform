8be0e3ba9304501fe22c873a0248a53f5d6762f8
RobDavenport <zerve0@hotmail.com>
2024-05-12 01:05:56 +0900

    progress

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,28 +1,44 @@
 // place files you want to import through the `$lib` alias in this folder.
 
-// TOOD: This
-// import { invoke } from "@tauri-apps/api/tauri";
-// import { readable } from "svelte/store";
+import { invoke } from '@tauri-apps/api/tauri';
 
-// export const globalTags = readable(undefined, function start(set) {
-// 	console.log("Getting global tags...")
-// 	invoke('plugin:tag|get_global_tags')
-// 		.then((arr) => {
-// 			set(arr)
-// 		})
-// 		.catch((err) => {
-// 			console.log(err)
-// 		})
-// });
+type GlobalTag = {
+	pid: Number;
+	name: string;
+};
 
-// export const permissionLevels = readable(undefined, function start(set) {
-// 	console.log("Getting permission levels...")
+type GlobalPermissionLevel = {
+	id: Number;
+	level_name: string;
+};
 
-// 	invoke('plugin:author|get_global_permission_levels')
-// 		.then((arr) => {
-// 			set(arr)
-// 		})
-// 		.catch((err) => {
-// 			console.log(err)
-// 		})
-// });
+let globalTags: GlobalTag[] = [];
+let globalPermissionLevels: GlobalPermissionLevel[] = [];
+
+export async function getGlobalTags(): Promise<GlobalTag[]> {
+	if (globalTags.length === 0) {
+		try {
+			globalTags = await invoke<GlobalTag[]>('plugin:tag|get_global_tags');
+		} catch (e) {
+			console.log(e);
+			return Promise.reject(e);
+		}
+	}
+
+	return Promise.resolve(globalTags);
+}
+
+export async function getGlobalPermissionLevels(): Promise<GlobalPermissionLevel[]> {
+	if (globalPermissionLevels.length === 0) {
+		try {
+			globalPermissionLevels = await invoke<GlobalPermissionLevel[]>(
+				'plugin:author|get_global_permission_levels'
+			);
+		} catch (e) {
+			console.log(e);
+			return Promise.reject(e);
+		}
+	}
+
+	return Promise.resolve(globalPermissionLevels);
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script>
 	import NavBar from '$lib/NavBar.svelte';
 	import '@picocss/pico/css/pico.indigo.min.css';
+	import { getGlobalPermissionLevels, getGlobalTags } from '$lib';
+	import { onMount } from 'svelte';
 
 	export const prerender = true;
 	export const ssr = false;
+
+	onMount(() => {
+		getGlobalTags();
+		getGlobalPermissionLevels();
+	});
 </script>
 
 <NavBar />
